b6682331151b631104cb8a0202bd46542c227578
EduardoJoaoTurkiewicz <eduardojunior1839@gmail.com>
2025-08-01 18:22:33 -0300

    Fix corrupted Agenda component

--- src/components/Agenda.tsx
+++ src/components/Agenda.tsx
@@ -1,1 +1,10 @@
-\\\\\\{"code"\:"rate-limited","message":"You have hit the rate lim\it. Please upgrade to keep chatting.","providerLimitHit": le\tsN"{xna){pp{l2 ePrP}t.r
+import React from 'react';
+
+export function Agenda() {
+  return (
+    <div>
+      <h1>Agenda</h1>
+      <p>Conteúdo da Agenda será implementado aqui.</p>
+    </div>
+  );
+}
